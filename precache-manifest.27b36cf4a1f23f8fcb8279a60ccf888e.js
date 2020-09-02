dbed77b0a1151abcfc55bba30d1b9275f248c670
ylm3112370004 <wintersweety@126.com>
2020-09-02 17:57:41 +0800

    deploy

---
+++ precache-manifest.27b36cf4a1f23f8fcb8279a60ccf888e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acb16733f554be6f592ae9cf27c6f16",
+    "url": "/mm-money-website/index.html"
+  },
+  {
+    "revision": "62ba8d232e767f0e6805",
+    "url": "/mm-money-website/static/css/main.e6bd5035.chunk.css"
+  },
+  {
+    "revision": "7d302c42f969615d8b55",
+    "url": "/mm-money-website/static/js/2.5defc190.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/mm-money-website/static/js/2.5defc190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ba8d232e767f0e6805",
+    "url": "/mm-money-website/static/js/main.e023e18e.chunk.js"
+  },
+  {
+    "revision": "2c3dc03b01c5690a765d",
+    "url": "/mm-money-website/static/js/runtime-main.9162a03f.js"
+  }
+]);
